626dba753e20874db97ed31f4989ed3680e3b416
miriam silva <miriam-silva@live.com>
2021-07-17 14:06:12 -0700

    Added tests

--- dotnet/Challenge05/Challenge05/Challenge05Test/UnitTest1.cs
+++ dotnet/Challenge05/Challenge05/Challenge05Test/UnitTest1.cs
@@ -26,5 +26,39 @@ namespace Challenge05Test
       Assert.Equal(5, OurList.Head.Value);
 
     }
+    [Fact]
+    public void HeadPointsToFirst()
+    {
+      LinkedList OurList = new LinkedList();
+      OurList.Insert(7);
+      OurList.Insert(8);
+      Assert.Equal(8, OurList.Head.Value);
+    }
+    [Fact]
+    public void TrueIfNameExists()
+    {
+      LinkedList OurList = new LinkedList();
+      OurList.Insert(3);
+      OurList.Insert(5);
+
+      Assert.True(OurList.Includes(5));
+    }
+    [Fact]
+    public void FalseIfNameDoesNotExists()
+    {
+      LinkedList OurList = new LinkedList();
+      OurList.Insert(3);
+      OurList.Insert(5);
+
+      Assert.False(OurList.Includes(4));
+    }
+    [Fact]
+    public void ReturnAllValues()
+    {
+      LinkedList OurList = new LinkedList();
+      OurList.Insert(2);
+      OurList.Insert(7);
+      Assert.Equal("(7) =>(2) =>", OurList.Print());
+    }
   }
 }
