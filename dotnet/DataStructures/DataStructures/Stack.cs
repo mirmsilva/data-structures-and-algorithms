0d308a039185a2788a006fbfd76c0c84ea4caf9b
miriam silva <miriam-silva@live.com>
2021-07-23 18:42:01 -0700

    Added the stack fucntion

--- dotnet/DataStructures/DataStructures/Stack.cs
+++ dotnet/DataStructures/DataStructures/Stack.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-  public class Stack
+  public class Stack <T>
   {
+    public Node<T> Top { get; set; }
+
+    //Pushes to the top of the stack
+    public void Push(T value)
+    {
+      Node<T> node = new Node<T>(value);
+      node.Next = Top;
+      Top = node;
+    }
+
+    //removes the node from the top of the stack
+    public Node<T> Pop()
+    {
+      Node<T> currentTop = Top;
+
+      Top = currentTop.Next;
+
+      return currentTop;
+    }
+
+    //Checks the top of the stack
+    public bool Peek()
+    {
+      return Top != null;
+    }
+
+    //Checks is the stack is empty
+    public bool IsEmpty()
+    {
+      if (Top != null)
+      {
+        return true;
+      }
+      return false;
+    }
   }
 }
