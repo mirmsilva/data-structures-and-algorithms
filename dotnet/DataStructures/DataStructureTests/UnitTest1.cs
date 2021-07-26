b5c2c16aee104baeba4fec53e14daae183e15350
miriam silva <miriam-silva@live.com>
2021-07-26 16:13:21 -0700

    Added PseudoQueue

--- dotnet/DataStructures/DataStructureTests/UnitTest1.cs
+++ dotnet/DataStructures/DataStructureTests/UnitTest1.cs
@@ -1,13 +1,26 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using DataStructures;
+
 
 namespace DataStructureTests
 {
   public class UnitTest1
   {
     [Fact]
-    public void Test1()
+    public void TestPseudoQueue()
     {
+      PseudoQueue<int> one = new PseudoQueue<int>();
+      one.stackOne.Push(5);
+      one.stackOne.Push(2);
+      one.stackOne.Push(7);
+      one.Enqueue(4);
+
+      PseudoQueue<int> two = new PseudoQueue<int>();
+
+      Assert.Equal(4, one.stackOne.Pop().Value);
+      
 
     }
   }
